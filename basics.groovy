bc357c53a85c2b72282e8dd9535122d14dce3805
Alex Momotov <alex.momotov@vonage.com>
2020-03-01 02:01:49 +0000

    Groovy basics.

---
+++ basics.groovy
@@ -0,0 +1,16 @@
+// Execute Groovy script:
+// groovy basics.groovy
+
+// Just like java code can be in main()
+// But it can also be outside classes like Python script
+
+class GroovyBasics {
+    public static void main(String[] args) {
+
+        // printing
+        println("Hello world");
+        System.out.println("Hello world");
+
+
+    }
+}
